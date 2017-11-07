bd87de15a7de85046f6e4597050f5b777c4940e6
Shefali Agarwal <sa652@cornell.edu>
2017-11-07 17:18:03 -0500

    added files

---
+++ includes/navigation.php
@@ -0,0 +1,12 @@
+<div class="links">
+  <h3>Links</h3>
+  <nav id="links">
+    <ul>
+      <li><a href="index.html">About Us</a></li>
+      <li><a href="team.html">Team Members</a></li>
+      <li><a href="pastperformances.html">Past Performances</a></li>
+      <li><a href="upcomingperformances.html">Upcoming Performances</a></li>
+      <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+  </nav>
+</div>
---
+++ upcomingperformances.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>E. Motion</title>
+  <link rel="stylesheet" type="text/css" href="styles/desktop.css" media="screen"/>
+  <link rel="stylesheet" type="text/css" href="styles/mobile.css" media="screen"/>
+</head>
+<body>
+  <h1>E. Motion</h1>
+  <?php
+    include "includes/navigation.php";
+  ?>
+</body>
+</html>
