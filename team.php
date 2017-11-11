825b00b18fe020d27b60c7a56af97710b5582757
Jason Duong <jasonduong@dhcp-mcfad-11003.eduroam.cornell.edu>
2017-11-11 12:38:32 -0500

    team photos and css

--- team.php
+++ team.php
@@ -23,29 +23,79 @@
     include "includes/navigation.php";
   ?>
   </header>
-  
-  <h2> Team Members </h2>
 
-  <h3> Executive Board </h3>
-    <figure>
-      <img src="photo.jpg" alt="Portrait photo" />
-      <figcaption>
-        Name, major, year, role
+  <!-- All photos on this page were provided by E. Motion -->
+  <h2> Executive Board </h2>
+  <div class="memberRow">
+    <figure class="teamMember">
+      <img src="images/Tiffany_Chan.jpg" alt="Portrait photo" />
+      <figcaption class="team_caption_name">
+        Tiffany Chan <span>Major</span> <span>Year</span> <span>Role</span>
       </figcaption>
     </figure>
 
-    <figure>
-      <img src="photo.jpg" alt="Portrait photo" />
-      <figcaption>
-        Name, major, year, role
+    <figure class="teamMember">
+      <img src="images/Annie_Xu.jpg" alt="Portrait photo" />
+      <figcaption class="team_caption_name">
+        Annie Xu <span>Major</span> <span>Year</span> <span>Role</span>
       </figcaption>
     </figure>
 
-    <figure>
-      <img src="photo.jpg" alt="Portrait photo" />
-      <figcaption>
-        Name, major, year, role
+    <figure class="teamMember">
+      <img src="images/Betty_Wang.jpg" alt="Portrait photo" />
+      <figcaption class="team_caption_name">
+        Betty Wang <span>Major</span> <span>Year</span> <span>Role</span>
       </figcaption>
     </figure>
+  </div>
+
+  <div class="memberRow">
+    <figure class="teamMember">
+      <img src="images/Christina_Lu.jpg" alt="Portrait photo" />
+      <figcaption class="team_caption_name">
+        Christina Lu <span>Major</span> <span>Year</span> <span>Role</span>
+      </figcaption>
+    </figure>
+
+    <figure class="teamMember">
+      <img src="images/Han_Chao_li.jpg" alt="Han Chao Li Portrait photo" />
+      <figcaption class="team_caption_name">
+        Han Chao Li <span>Major</span> <span>Year</span> <span>Role</span>
+      </figcaption>
+    </figure>
+
+    <figure class="teamMember">
+      <img src="images/Jaewon_Baek.jpg" alt="Jaewon Baek Portrait photo" />
+      <figcaption class="team_caption_name">
+        Jaewon Baek <span>Major</span> <span>Year</span> <span>Role</span>
+      </figcaption>
+    </figure>
+  </div>
+
+  <div class="memberRow">
+    <figure class="teamMember">
+      <img src="images/Jennifer_Lin.jpg" alt="Jennifer Lin Portrait photo" />
+      <figcaption class="team_caption_name">
+        Jennifer Lin <span>Major</span> <span>Year</span> <span>Role</span>
+      </figcaption>
+    </figure>
+
+    <figure class="teamMember">
+      <img src="images/Michelle_Wang.jpg" alt="Michelle Wang Portrait photo" />
+      <figcaption class="team_caption_name">
+        Michelle Wang <span>Major</span> <span>Year</span> <span>Role</span>
+      </figcaption>
+    </figure>
+  </div>
+  <hr/>
+
+  <h2> The E. Motion Team </h2>
+  <figure id="team_photo">
+    <img src="images/Emotion_group.jpg" alt="Group Halloween 2017 Photo" />
+    <figcaption id="group_photo_caption">
+      Halloween 2017
+    </figcaption>
+  </figure>
+
 </body>
 </html>
