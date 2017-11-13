ee08dfbe109afc86327c038d01d6a345f61af856
Shefali Agarwal <sa652@cornell.edu>
2017-11-13 13:57:39 -0500

    removed aboutus.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,7 +2,6 @@
   <nav id="links">
     <ul>
       <li><a href="index.php">Home</a></li>
-      <li><a href="aboutus.php">About Us</a></li>
       <li><a href="team.php">Team Members</a></li>
       <li><a href="pastperformances.php">Past Performances</a></li>
       <li><a href="upcomingperformances.php">Upcoming Performances</a></li>
